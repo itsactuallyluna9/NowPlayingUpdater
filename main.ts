7e77873ba8ec8e390a854ee2f87d6a0d6fe24e36
Luna <itsactuallyluna9@gmail.com>
2024-09-30 19:53:09 -0500

    add support for latest cider api

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ import {
   brightRed,
 } from "https://deno.land/std@0.224.0/fmt/colors.ts";
 
-console.log("Now Playing Updater v0.0.3");
+console.log("Now Playing Updater v0.1.0-git");
 console.log(
-  "For Bean, Made with" + brightRed(" <3 ") + "by" + brightCyan(" Luna"),
+  "Made with" + brightRed(" <3 ") + "by" + brightCyan(" Luna"),
 );
 console.log("Now starting...");
 
@@ -27,14 +27,67 @@ function getCurrentShowName() {
   }
 }
 
-async function updateNowPlaying() {
+async function updateNowPlayingNeo() {
+  try {
+    const res = await fetch("http://localhost:10767/api/v1/playback/now-playing", {
+      headers: {
+        'apptoken': 'lunaiscool'
+      }
+    });
+    const data = await res.json();
+    if (data.error && data.error === "Unauthorized") {
+      console.log("Unauthorized! To fix this, create a new token and replace it with 'lunaiscool'. (A proper way to do this is coming soon!)")
+      console.log("You may need to restart Cider afterwords.")
+      return;
+    }
+    if (data.info.error && data.info.error === "Timeout exceeded") {
+      console.log("Timeout exceeded! This is likely a bug in Cider, trying again!")
+      setTimeout(updateNowPlayingNeo, 1000);
+      return;
+    }
+    errors = 0;
+    if (data.info.isrc === previousISRC) {
+      if (data.info.remainingTime < 10) { // 10 seconds
+        setTimeout(updateNowPlayingNeo, (data.info.remainingTime * 1000) + 500);
+      }
+      return;
+    }
+    previousISRC = data.info.isrc;
+    if (data.info.name === undefined || data.info.artistName === undefined) {
+      console.log("Failed to get song info, but health is ok! (is nothing playing currently?)")
+      console.log("Providing a default message...");
+      await Deno.writeFile("now_playing.txt", new TextEncoder().encode(getCurrentShowName()));
+      return;
+    }
+    await Deno.writeFile(
+      "now_playing.txt",
+      new TextEncoder().encode(data.info.name + " - " + data.info.artistName),
+    );
+    console.log(
+      "Updated now playing to " + data.info.name + " - " + data.info.artistName,
+    );
+  }
+  catch (e) {
+    console.log(e)
+    if (errors > 5) {
+      console.error("Too many errors, exiting...");
+      Deno.exit(1);
+    }
+    console.error("Failed to update! Retrying in 10 seconds...");
+    await Deno.writeFile("now_playing.txt", new TextEncoder().encode(getCurrentShowName()));
+    errors++;
+    return;
+  }
+}
+
+async function updateNowPlayingOld() {
   try {
     const res = await fetch("http://localhost:10769/currentPlayingSong");
     const data = (await res.json()).info;
     errors = 0;
     if (data.isrc === previousISRC) {
       if (data.remainingTime < 10_000) { // 10 seconds
-        setTimeout(updateNowPlaying, data.remainingTime + 500);
+        setTimeout(updateNowPlayingOld, data.remainingTime + 500);
       }
       return;
     }
@@ -70,5 +123,41 @@ async function updateNowPlaying() {
   }
 }
 
-setInterval(updateNowPlaying, 10_000);
-updateNowPlaying();
+async function boot() {
+  // wait until either of the two servers are up
+  let attempts = 0;
+  let updateNowPlaying: () => Promise<void>;
+  while (true) {
+    try {
+      const res = await fetch("http://localhost:10767/api/v1/playback/active")
+      if (res.status === 200 || res.status === 403) {
+        console.log("Using new API.");
+        updateNowPlaying = updateNowPlayingNeo;
+        break;
+      }
+    } catch (_e) {
+      // ignore
+    }
+    try {
+      const res2 = await fetch("http://localhost:10769/currentPlayingSong");
+      if (res2.status === 200) {
+        console.log("Using old API.");
+        updateNowPlaying = updateNowPlayingOld;
+        break;
+      }
+    } catch (_e) {
+      // ignore
+    }
+    attempts++;
+    if (attempts > 10) {
+      console.error("Failed to connect to either API! Exiting...");
+      Deno.exit(1);
+    }
+    console.log("Waiting on Cider...");
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+  await updateNowPlaying()
+  setInterval(updateNowPlaying, 10_000);
+}
+
+await boot();
